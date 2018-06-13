b08896280151bab5186a3b20edc7958b06a62d66
martensnico <nicomartens89@hotmail.com>
2018-06-13 08:43:05 +0200

    Added automatic download of Skype module and sign in assistant

--- Office365.Connect/Private/Get-Prerequisite.ps1
+++ Office365.Connect/Private/Get-Prerequisite.ps1
@@ -25,9 +25,23 @@ function Get-Prerequisite
 	}
 	else
 	{
+		Write-Host ("Microsoft Online Services Sign-in Assistant for IT Professionals RTW missing") -Fore Red
+		$title = "Install Microsoft Online Services Sign-in Assistant for IT Professionals RTW?"
+		$message = "Do you want to download and install the Microsoft Online Services Sign-in Assistant for IT Professionals automatically?"
+		$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes"
+		$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No"
+		$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes,$no)
+		$choice = $host.UI.PromptForChoice($title,$message,$options,1)
+
+		if($choice -eq 0)
+		{
+			Get-SigninAssistant
+		}
+		else{
 		Write-Host ("Microsoft Online Services Sign-in Assistant for IT Professionals RTW missing, please download here:") -Fore Red
 		Write-Host ("https://www.microsoft.com/en-us/download/details.aspx?id=28177") -Fore Yellow
 		$state = 0
+		}
 	}
 
 	#If services is missing and modules
@@ -49,19 +63,13 @@ function Get-Prerequisite
 
 		if ($choice -eq 0)
 		{
-			if ([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544"))
+			if (Get-CurrentPrivilege -eq $true)
 			{
-				Write-Host ("Current instance is running as administrator") -Fore Green
-
 				foreach ($module in $missingmodules)
 				{
 					if($module -eq "SkypeOnlineConnector")
 					{
-						Write-Host ("Unfortunately, the Skype for Business Online PowerShell module must be downloaded manually.") -Fore Red
-						Write-Host ("You can do this here:") -Fore Red
-						Write-Host ("https://www.microsoft.com/en-us/download/details.aspx?id=39366") -Fore Yellow
-						WaitAnyKey
-						exit
+						Get-S4BModule
 					}
 					else{
 						Write-Host ("Installing module $module") -Fore Yellow
@@ -69,14 +77,50 @@ function Get-Prerequisite
 					}
 				}
 			}
-			else {
-				Write-Host ("Please start PowerShell as administrator to install modules") -Fore Red
-				WaitAnyKey
-				exit
-			}
 		}
 		Clear-Host
 		Get-Prerequisite
 	}
 	Clear-Host
+}
+
+function Get-CurrentPrivilege
+{
+	$privilege = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+	if($privilege -eq $true){Write-Host ("Current instance is running as administrator") -Fore Green}
+	else{Write-Host ("Please start PowerShell as administrator to install modules") -Fore Red; WaitAnyKey; exit}
+	return $privilege
+}
+
+function Get-SigninAssistant
+{
+	if(Get-CurrentPrivilege -eq $true)
+	{
+	Write-Host("Downloading Microsoft Online Services Sign-in Assistant for IT Professionals RTW") -Fore Yellow
+	$URL = "https://download.microsoft.com/download/7/1/E/71EF1D05-A42C-4A1F-8162-96494B5E615C/msoidcli_64bit.msi"
+	$Filename = $URL.Split('/')[-1]
+	Invoke-WebRequest -Uri $URL -UseBasicParsing -OutFile "$env:TEMP\$Filename" 
+
+	Write-Host("Installing Microsoft Online Services Sign-in Assistant for IT Professionals RTW") -Fore Yellow
+	& $env:TEMP\$Filename /qn
+	Write-Host("Microsoft Online Services Sign-in Assistant for IT Professionals RTW has been installed") -Fore Green
+	}
+}
+
+function Get-S4BModule
+{
+	if(Get-CurrentPrivilege -eq $true)
+	{
+	Write-Host("Downloading Skype for Business Online Powershell module") -Fore Yellow
+	$URL = "https://download.microsoft.com/download/2/0/5/2050B39B-4DA5-48E0-B768-583533B42C3B/SkypeOnlinePowerShell.Exe"
+	$Filename = $URL.Split('/')[-1]
+	$file = "$env:TEMP\$Filename" 
+	Write-Host $file
+	Invoke-WebRequest -Uri $URL -UseBasicParsing -OutFile $file
+
+	Write-Host("Installing Skype for Business Online Powershell module") -Fore Yellow
+
+	. $file /S /v /qn
+	Write-Host("Skype for Business Online Powershell module has been installed") -Fore Green
+	}
 }
